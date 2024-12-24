361e1f878a5f8bd47aeaf1e680b37b1c8976685b
colonelmustard-f1 <marioarjona7@gmail.com>
2024-12-23 22:45:15 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RaceResults from './components/RaceResults'
 
 function App() {
   return (
-    <div className="App">
-      <h1>WSC F1 Game</h1>
+    <div className="App p-4">
+      <h1 className="text-2xl font-bold mb-4">WSC F1 Game</h1>
+      <RaceResults />
     </div>
   )
 }
